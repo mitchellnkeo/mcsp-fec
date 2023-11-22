afd936afbc21f71078a5611e2a9c1dd2aa0bbb96
Quinn Kaji <quinnkaji@gmail.com>
2023-11-22 13:10:36 -1000

    added another table.

--- server/migration.sql
+++ server/migration.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS decks;
 DROP TABLE IF EXISTS flashcard;
+DROP TABLE IF EXISTS decks;
 
 CREATE TABLE decks (
   id SERIAL PRIMARY KEY,
@@ -14,11 +14,13 @@ CREATE TABLE flashcard(
 
 );
 INSERT INTO decks(name,description) VALUES('REACT','REACT VOCAB DECK');
+INSERT INTO decks(name,description) VALUES('POSTGRESQL','POSTGRESL VOCAB DECK');
 
 
 
 INSERT INTO flashcard(question,answer,deck_id) VALUES('What is REACT?','It is a lbirary using',1);
 INSERT INTO flashcard(question,answer,deck_id) VALUES('What is a hook?','It is a way for us to make varriables passed through components',1);
+INSERT INTO flashcard(question,answer,deck_id) VALUES('What is POSTGRESQL','ANSWER',2);
 
 
 -- to access data in the table use this command in SQL
