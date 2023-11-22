f383a7bbd76cacb4b448470cecee37199e42c691
Quinn Kaji <quinnkaji@gmail.com>
2023-11-22 11:31:38 -1000

    created tables and data structure

--- server/migration.sql
+++ server/migration.sql
@@ -1,12 +1,25 @@
-DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS decks;
+DROP TABLE IF EXISTS flashcard;
 
-CREATE TABLE tasks (
-  id SERIAL,
+CREATE TABLE decks (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
   description TEXT
 );
+CREATE TABLE flashcard(
+  id SERIAL PRIMARY KEY,
+  question TEXT,
+  answer TEXT,
+  deck_id INT REFERENCES decks(id)
 
-INSERT INTO tasks(description) VALUES('Do the dishes');
-INSERT INTO tasks(description) VALUES('Walk the dog');
-INSERT INTO tasks(description) VALUES('Sweep the floor');
-INSERT INTO tasks(description) VALUES('Do your homework');
-INSERT INTO tasks(description) VALUES('Beat Elden Ring');
+);
+INSERT INTO decks(name,description) VALUES('REACT','REACT VOCAB DECK');
+
+
+
+INSERT INTO flashcard(question,answer,deck_id) VALUES('What is REACT?','It is a lbirary using',1);
+INSERT INTO flashcard(question,answer,deck_id) VALUES('What is a hook?','It is a way for us to make varriables passed through components',1);
+
+
+-- to access data in the table use this command in SQL
+-- SELECT*FROM decks INNER JOIN flashcard ON decks.id = deck_id;
