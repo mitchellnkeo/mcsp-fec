19d04fb8812f678fa559c269380dd3631b0351a5
John Gordinier <jsgordinier@gmail.com>
2023-11-27 16:13:51 -0600

    Deck and About Pages

---
+++ client/components/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Galvanize Vocab</h1>
+      <p>
+        The brilliant minds at Operation Level Up, fueled by caffeine and the
+        occasional snack-induced brainstorm, didn't just stop at crafting your
+        run-of-the-mill flashcard app. Oh no! They took it up a notch and
+        decided, "Hey, let's sprinkle some programming magic in there!"
+      </p>
+      <p>
+        These students are not just coding a Quizlet look-alike; they're on a
+        mission to turn every coding novice into a programming pro, one
+        flashcard at a time. It's like they looked at traditional study methods
+        and said, "Boring! Let's make learning to code as fun as trying to
+        explain memes to our grandparents."
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ client/components/Decks.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+
+const Decks = () => {
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    fetch("/FEC/decks")
+      .then((res) => res.json())
+      .then((decks) => {
+        setDecks(decks);
+      })
+      .catch((error) => {
+        console.error("Error fetching decks:", error);
+      });
+  }, []);
+
+  const handleDeckClick = (deckNumber) => {
+    fetch(`/FEC/decks/${deckNumber}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error fetching deck:", error));
+  };
+
+  const renderDeckBoxes = () => {
+    const deckBoxes = [];
+    const rows = 3;
+    const columns = 4;
+
+    for (let i = 1; i <= rows; i++) {
+      for (let j = 1; j <= columns; j++) {
+        const deckIndex = (i - 1) * columns + j - 1;
+        const deck = deckIndex < decks.length ? decks[deckIndex] : null;
+
+        const deckBoxStyle = {
+          margin: "10px",
+          cursor: "pointer",
+          width: "400px",
+          height: "175px",
+          backgroundColor: "lightblue",
+          display: "inline-block",
+        };
+
+        deckBoxes.push(
+          <div
+            key={deck ? `deck${deck.id}` : `emptyDeck${deckIndex}`}
+            className="deck-in-the-box"
+            style={{
+              ...deckBoxStyle,
+              marginLeft: j > 1 ? "10px" : "0",
+            }}
+            onClick={() => handleDeckClick(deck ? deck.id : null)}
+          >
+            {deck ? `Deck ${deck.id}` : `Empty Deck ${deckIndex + 1}`}
+            {deck && <p>{deck.description}</p>}{" "}
+            {/* Add this line for deck description */}
+          </div>
+        );
+      }
+    }
+
+    return deckBoxes;
+  };
+
+  const containerStyle = {
+    marginTop: "200px",
+    textAlign: "center",
+  };
+
+  return (
+    <div style={containerStyle}>
+      <main>{renderDeckBoxes()}</main>
+    </div>
+  );
+};
+
+export default Decks;
