a877d350e04fcc5c373381a70a28a81c1a626d15
Mitchell Keo <143657298+mitchellnkeo@users.noreply.github.com>
2023-11-29 14:12:54 -0800

    Mk/local storage (#21)
    * Worked out the flow from Question/Local Storage/
    Deck
    
    * Morning push.
    
    * Worked out the flow from Question/Local Storage/
    Deck
    
    * Moved all my stuff to separate file.
    
    * Updatec Decks component.
    
    * Updated handleDeckClick
    
    * Last few changes before PR.
    
    * mk commit to Master.

--- client/components/Decks.jsx
+++ client/components/Decks.jsx
@@ -1,61 +1,41 @@
 import React, { useEffect, useState } from "react";
+import styles from "./deck.module.css";
 
 const Decks = () => {
+  // State being used for the decks rendering on the Decks page
   const [decks, setDecks] = useState([]);
 
+  const [questions, setQuestions] = useState([])
+
+  // HANDLER FOR SPECIFIC DECK ID
+  // 
   const handleDeckClick = (deckNumber) => {
-    fetch(`/api/decks/${deckNumber}`)
+    fetch(`/api/deck/${deckNumber}`)
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+      setQuestions(data)})
       .catch((error) => console.error("Error fetching deck:", error));
   };
 
   const renderDeckBoxes = () => {
-    const deckBoxes = [];
-    const rows = 3;
-    const columns = 4;
-
-    for (let i = 1; i <= rows; i++) {
-      for (let j = 1; j <= columns; j++) {
-        const deckIndex = (i - 1) * columns + j - 1;
-        const deck = deckIndex < decks.length ? decks[deckIndex] : null;
-
-        const deckBoxStyle = {
-          margin: "10px",
-          cursor: "pointer",
-          width: "400px",
-          height: "175px",
-          backgroundColor: "lightblue",
-          display: "inline-block",
-        };
-
-        deckBoxes.push(
-          <div
-            key={deck ? `deck${deck.id}` : `emptyDeck${deckIndex}`}
-            className="deck-in-the-box"
-            style={{
-              ...deckBoxStyle,
-              marginLeft: j > 1 ? "10px" : "0",
-            }}
-            onClick={() => handleDeckClick(deck ? deck.id : null)}
-          >
-            {deck ? `Deck ${deck.id}` : `Empty Deck ${deckIndex + 1}`}
-            {deck && <p>{deck.description}</p>}{" "}
-            {/* Add this line for deck description */}
-          </div>
-        );
-      }
-    }
-
-    return deckBoxes;
+    return decks.map((deck, index) => (
+      <div
+        key={`deck${deck.id}`}
+        className={styles["deck-box"]}
+        onClick={() => handleDeckClick(deck.id)}
+      >
+        {`Deck ${deck.id}`}
+        <p>{deck.description}</p>
+      </div>
+    ));
   };
-
+  
   const containerStyle = {
     textAlign: "center",
   };
-
+  
   useEffect(() => {
-    fetchDecks(); // Call fetchDecks function within the component
+    fetchDecks();
   }, []);
 
   const fetchDecks = () => {
@@ -73,5 +53,4 @@ const Decks = () => {
     </div>
   );
 };
-
 export default Decks;
