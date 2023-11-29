a1a07c89cc9c1084e2571f1234e6120cf40915a8
Scott Shannon <78231948+shannonscotta@users.noreply.github.com>
2023-11-29 15:50:44 -0800

    save point before rebase

--- client/components/Home/Home.jsx
+++ client/components/Home/Home.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
+  let homeParagraphString = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+  ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+  aliquip ex ea commodo consequat. Duis aute irure dolor.`;
+
   return (
     <>
-    <img className="galvanize-guy-png" src="galvanize-dude.png" />
+      <div className={styles["home-container"]}>
+        <img className={styles["galvanize-guy-png"]} src="galvanize-dude.png" />
 
-    <div className="landing-content">
-    <div className="landing-title">Transforming Vocabulary Study</div>
-    <div className="landing-paragraph"></div>
-    </div>
+        <div className={styles["home-text-container"]}>
+          <div className={styles["home-title"]}>
+            Transforming <br /> Vocabulary Study
+          </div>
+          <div className={styles["home-paragraph"]}>{homeParagraphString}</div>
+        </div>
+      </div>
     </>
   );
 };
