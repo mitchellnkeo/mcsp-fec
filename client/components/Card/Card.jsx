476ca46597548f1d526303278363d03a5d29845c
John Gordinier <jsgordinier@gmail.com>
2023-12-01 08:45:39 -0600

    John-finished reveal answer button function

--- client/components/Card/Card.jsx
+++ client/components/Card/Card.jsx
@@ -3,22 +3,32 @@ import styles from "./Card.module.css";
 
 const Card = ({ deckArr }) => {
   const [cardIndex, setCardIndex] = useState(0);
+  const [revealAnswer, setRevealAnswer] = useState(false);
 
   let deckCount = deckArr.length;
 
   let handleClick = () => {
-    if (cardIndex < deckCount - 1) setCardIndex(cardIndex + 1);
+    if (cardIndex < deckCount - 1) {
+      setCardIndex(cardIndex + 1);
+      setRevealAnswer(false);
+    }
   };
 
   return (
     <div className={styles.card}>
       <p>{deckArr[cardIndex].question}</p>
-      <br />
-      <p>{deckArr[cardIndex].answer}</p>
-      {/* <p>id: {deckArr[cardIndex].id}</p> */}
-      <button className={styles["answer-button"]} onClick={handleClick}>
+      {revealAnswer && <p>{deckArr[cardIndex].answer}</p>}
+      <button
+        className={styles["answer-button"]}
+        onClick={() => setRevealAnswer(true)}
+      >
         REVEAL ANSWER
       </button>
+      {revealAnswer && (
+        <button className={styles["next-card-button"]} onClick={handleClick}>
+          NEXT QUESTION
+        </button>
+      )}
     </div>
   );
 };
